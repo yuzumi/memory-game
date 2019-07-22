81a74dca498b352202ef4a63b28f9857101cf06a
Bogdan <bogdan7morozyuk@gmail.com>
2019-07-22 12:06:04 +0300

    Display board with cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useState, useEffect } from 'react';
+import Board from 'components/board';
+import initialize, { ICard } from 'services/card';
 
-const App: FunctionComponent = () => (
-  <div className="app"></div>
-);
+const App: FunctionComponent = () => {
+  const [ cards, setCards ] = useState<ICard[]>([]);
+  const [ flipped, setFlipped ] = useState<string[]>([]);
+
+  const handleClick = (id: string) => {
+    setFlipped([ ...flipped, id ]);
+  };
+
+  useEffect(() => {
+    setCards(initialize());
+  }, []);
+
+  return (
+    <div className="app">
+      <Board 
+        cards={cards}
+        flipped={flipped}
+        onClick={handleClick}
+      />
+    </div>
+  );
+};
 
 export default App;
